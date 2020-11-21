5f536b8874b5b4ba5e7f8f21f428364321b2b89b
Eren Şimşek <scafereren@gmail.com>
2020-11-21 15:29:06 +0300

    son

--- python_server/server.py
+++ python_server/server.py
@@ -13,14 +13,16 @@ def tweets():
     auth.set_access_token(config.access_token_key, config.access_token_secret)
     api = tweepy.API(auth)
 
-    tweets = api.search('#covid19', count=100)
+    tweets = api.search('#covid19', count=1)
 
     result = []
     token_list = []
+    ids = []
     stop_words = set(nltk.corpus.stopwords.words('english'))
 
     for tweet in tweets:
-        tokens = nltk.word_tokenize(re.sub('[^A-Za-z0-9]+', ' ', tweet.text))
+        tokens = nltk.word_tokenize(
+            re.sub('[^A-Za-z0-9]+', ' ', tweet.text.lower()))
         for token in tokens:
             if(token == 'RT' or '@' in token):
                 continue
@@ -28,7 +30,10 @@ def tweets():
                 token_list.append(token)
 
     for token in token_list:
-        result.append({'text': token, 'value': token_list.count(token)})
+        if token in token_list:
+            token_list[token] += 1
+        else:
+            token_list[token] = 1
 
     if(len(result) > 50):
         result = result[0:50]
